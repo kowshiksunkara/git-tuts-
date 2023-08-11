6430301a604739ffa771656ae39c0d3748925ee5
kowshiksunkara <140795606+kowshiksunkara@users.noreply.github.com>
2023-08-11 13:21:10 +0530

    Add files via upload

---
+++ oddoreven.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    clrscr();
+    printf("enter a number");
+    scanf("%d",%&a);
+    if(a%2==0)
+    {
+        printf("given number is even");
+    }
+    else
+    {
+        printf("given number is odd");
+
+    }
+    getch();
+}
